36f677dbd440bdf0dd9b7644c3a2d7eac87231c3
melanycpm1 <melanycatalanpabon@gmail.com>
2025-10-09 17:56:11 -0300

    feat:agregar formulario personajes

--- app/src/main/java/com/example/wikison/MainActivity.java
+++ app/src/main/java/com/example/wikison/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.wikison;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +14,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        LinearLayout contenedor = findViewById(R.id.contenedor_edittexts);
+
+        // Lista de textos diferentes
+        String[] hints = {"Nombre", "rol", "caracteristica","url", "frase"};
+
+        for (String hint : hints) {
+            EditText editText = new EditText(this);
+
+            // Atributos iguales a los del XML original
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    400, //
+                    200
+            );
+            params.setMargins(0, 25, 0, 0);
+            editText.setLayoutParams(params);
+
+            editText.setHint(hint);
+            editText.setBackgroundResource(R.drawable.edittext_border);
+            editText.setPadding(16, 16, 16, 16);
+            editText.setTextSize(16);
+            editText.setTypeface(editText.getTypeface(), android.graphics.Typeface.BOLD);
+            editText.setTextColor(getColor(android.R.color.black));
+
+            // Agregamos al contenedor
+            contenedor.addView(editText);
+        }
     }
 }
